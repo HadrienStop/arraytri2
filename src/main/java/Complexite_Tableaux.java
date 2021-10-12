337689815189de5a54e6899bbeca4f539d56be08
s20025226 <hadrienlma@gmail.com>
2021-10-12 09:17:51 +0200

    import triinsertion write file .dat

--- src/main/java/Complexite_Tableaux.java
+++ src/main/java/Complexite_Tableaux.java
@@ -15,13 +15,17 @@ public class Complexite_Tableaux
  
      SelectionSortedArrayWithCounting tab = new SelectionSortedArrayWithCounting();
      
-     tab.fillwithRandom(40,20); 
-     
-     
-     System.out.println(tab.toString()); 
-     
-     tab.SelectionSorting(); 
-     
+     tab.fillwithRandom(40,20);
+
+     Writer file = new FileWriter("triInsertion.dat");
+
+     tab.SelectionSorting();
+
+     for(int i = 0 ; i < tab.length() ; i++) {
+       file.write("" + String.valueOf(tab.getreadCount()) + " " + String.valueOf(tab.getwriteCount()) + " " + String.valueOf(tab.getlessCount()) + "\n");
+     }
+     file.close();
+
      System.out.println(tab.toString());
      
      System.out.println("nombre de lectures : "
@@ -32,8 +36,7 @@ public class Complexite_Tableaux
            
      System.out.println("nombre de comparaisons : "
            +String.valueOf(tab.getlessCount()));         
-         
-  }
+    }
 }
     
     
